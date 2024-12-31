77f280c1039b4d125703e94b5bdaad4498dafc99
9mz1 <muzyyyyyyy@gmail.com>
2024-12-31 19:09:08 +0400

    Added Computer choice and prompt for Human choice

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-// console.log('Active');
+// rock defeats scissors scissors defeats paper paper defeats rock
 
+
+
+//Score
+let humanScore = 0;
+let computerScore = 0;
+
+
+// get computer choice
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3);
+    return computerChoice;
+}
+
+// get human choice
+function getHumanChoice() {
+    let humanChoice = prompt('Rock | Paper | Scissors').toLowerCase();
+    if (humanChoice == 'rock') {
+        humanChoice = 0;
+    } else if (humanChoice == 'paper') {
+        humanChoice = 1;
+    } else if (humanChoice == 'scissor') {
+        humanChoice = 2;
+    } else {
+        alert('WRONG INPUT!')
+    }
+
+    // console.log(`Rock | Paper | Scissor: ${humanChoice}`);
+}
+
+
+// Game Logic
